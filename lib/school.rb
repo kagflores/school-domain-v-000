9875b3187d3d5096d5e90cd7f65eb5911a5afd58
Karen Flores <karen.ag.flores@gmail.com>
2018-05-17 02:36:44 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+
+class School
+attr_reader :name, :grade, :student
+  def initialize(name)
+    @name=name
+  end
+
+  def roster
+    roster = {}
+  end
+
+  def add_student(student, grade)
+    @student=student
+    @grade=grade
+    
+    if roster.has_key?(grade)
+      binding.pry
+      roster[grade] << student
+    else
+      roster[grade] = grade
+      roster[grade] = []
+      roster[grade] << name
+    end
+  end
+end
